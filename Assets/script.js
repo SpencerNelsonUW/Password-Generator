af7faf4f3da9e02589910e27d4c138af71488a0b
SpencerNelsonUW <spencernelsonn@gmail.com>
2022-07-24 18:50:08 -0700

    fixing the password result display and commenting out the code

--- Assets/script.js
+++ Assets/script.js
@@ -1,58 +1,83 @@
-
+//predetermined variables
 var lowercase = ("abcdefghijklmnopqrstuvwxyz");
 var uppercase = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 var numeral = ("0123456789");
 var specialchars = ("!@#$%^&*()");
 
-var userinputs;
-var passwordresult = "";
-
+//splits my predetermined string within my variable into an array, (less typing "")
 lowercase = lowercase.split("")
 uppercase = uppercase.split("")
 numeral = numeral.split("")
 specialchars = specialchars.split("")
 
+//setting up places for user input and results to be stored
+var userinputs = "";
+var passwordresult = "";
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+//this is the function that asks the user prompts and generates the password
+function generatePassword() {
+  let usercharacteramount = prompt("Choose choose between 8 and 128 for password length") //asking the user for password length
 
-// Write password to the #password input
-function writePassword() {
-  let usercharacteramount = prompt("Choose choose between 8 and 128 for password length")
-    if (usercharacteramount > 7 && usercharacteramount < 129){
-      let userlowercase = prompt("would you like to include lowercase in your password?")
-      let useruppercase = prompt("would you like to include uppercase in your password?") 
-      let usernumeral = prompt("would you like to include numbers in your password?")
-      let userspecialchars = prompt("would you like to include special characters in your password?")
+    //checking that the user input a valid password length
+    if (usercharacteramount > 7 && usercharacteramount < 129){     
+
+      //asking the user to define which character types to use
+      let userlowercase = prompt("Answer yes or no, would you like to include lowercase in your password?")
+      let useruppercase = prompt("Answer yes or no, would you like to include uppercase in your password?") 
+      let usernumeral = prompt("Answer yes or no, would you like to include numbers in your password?")
+      let userspecialchars = prompt("Answer yes or no, would you like to include special characters in your password?")
+
+        //checking the user input at least one character type and alerting them if none were chosen
         if (userlowercase !== "yes" && useruppercase !== "yes" && usernumeral !== "yes" && userspecialchars !== "yes"){
           alert("please choose at least one character type for password generation")
+
+          //checking which character types to use based on user input above and adding it to a variable called userinput
         } else {
-            if (userlowercase.toLowerCase() == "yes"){
+            if (userlowercase.toLowerCase() == "yes"){    
               userinputs = lowercase;
             }
             if (useruppercase.toLowerCase() == "yes"){
               userinputs = userinputs.concat(uppercase)
             }
             if (usernumeral.toLowerCase() == "yes"){
-              userinputs = userinputs.concat(usernumeral)
+              userinputs = userinputs.concat(numeral)
             }
             if (userspecialchars.toLowerCase() == "yes"){
-              userinputs = userinputs.concat(userspecialchars)
+              userinputs = userinputs.concat(specialchars)
             }
 
-            for (let i = 0; i <= usercharacteramount; i++){
+            //chooses value randomly from userinput variable created above, and does that however many times as the user asked
+            for (let i = 0; i <= usercharacteramount - 1; i++){
               passwordresult = passwordresult + userinputs[Math.floor(Math.random() * userinputs.length)]
             }
+
+            //this is a test for the developer to see the result in console.
             console.log(passwordresult)
         }
+
+        //tells the user they entered an invalid length
       } else { 
           alert("please enter at a number between 8 and 128")
         }
-          
-
-  //var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+        
+//returns the password result to be used elsewhere
+return passwordresult;
 }
 
-// Add event listener to generate button
+//Assignment Code, this defines the part of the html that will be considered a variable in java
+var generateBtn = document.querySelector("#generate");
+
+//this tells java to listend for when the variable is clicked, and that it should run the generatePassword function
+generateBtn.addEventListener("click", generatePassword);
+
+//this tells java to listen for when the variable is clicked, and that it should run the writePassword function
 generateBtn.addEventListener("click", writePassword);
+
+//This writes my passwrod to the screen
+function writePassword() {
+  let userPassword = document.querySelector("#password");
+  userPassword.value = passwordresult;
+  alert("please refresh the page before generating another password")
+}
+
+
